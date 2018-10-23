246c7922c338e8f21752d19ddade7edcb2301194
samussiah <samussiah@gmail.com>
2018-10-23 13:12:31 -0400

    update package.json devDepencies, rollup.config.js, and location of test-page

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,41 @@
-import babel from "rollup-plugin-babel";
+import babel from 'rollup-plugin-babel';
+
+var pkg = require('./package.json');
 
 module.exports = {
-    "name": "aeTimelines",
-    "input": "./src/wrapper.js",
-    "output": {
-        "file": "./build/aeTimelines.js",
-        "format": "umd"
+    input: pkg.module,
+    output: {
+        name: pkg.name
+            .split('-')
+            .map((str,i) =>
+                i === 0 ?
+                    str :
+                    (str.substring(0,1).toUpperCase() + str.substring(1))
+            )
+            .join(''),
+        file: pkg.main,
+        format: 'umd',
+        globals: {
+            d3: 'd3',
+            webcharts: 'webCharts'
+        },
     },
-    "globals": {
-        "d3": "d3",
-        "webcharts": "webCharts"
-    },
-    "external": (function() {
-        var dependencies = require("./package.json").dependencies;
+    external: (function() {
+        var dependencies = pkg.dependencies;
 
         return Object.keys(dependencies);
     }()),
-    "plugins": [
+    plugins: [
         babel({
-            "exclude": "node_modules/**",
-            "presets": [
-                ["env",
-                {"modules": false}
-                ]
+            exclude: 'node_modules/**',
+            presets: [
+                [ 'env', {modules: false} ]
             ],
-            "plugins": [
-                "external-helpers"
+            plugins: [
+                'external-helpers'
             ],
-            "babelrc": false
+            babelrc: false
         })
     ]
 };
+
